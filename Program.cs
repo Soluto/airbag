c674e651343bfa9e7b4839db23b0dd7810649c6b
Itay Maoz <itay@soluto.com>
2018-02-07 17:29:37 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace airbag
+{
+    public class Program
+    {
+        private static IConfigurationRoot Configuration;
+
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            Configuration = builder.Build();
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+             .ConfigureMetricsWithDefaults(
+                builder =>
+                {
+                    builder.Configuration.Configure(
+                        options =>
+                        {
+                            options.Enabled = Configuration.GetValue<bool>("COLLECT_METRICS");
+                        });
+                })
+                .UseMetrics(options =>
+                {
+                    options.EndpointOptions = endpointsOptions =>
+                                {
+                                    endpointsOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+                                    endpointsOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+                                };
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace airbag
+{
+    public class Startup
+    {
+        private IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                {
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidIssuer = Configuration.GetValue<string>("ISSUER"),
+                        ValidateAudience = false
+                    };
+                    options.Authority = Configuration.GetValue<string>("AUTHORITY");
+                    options.RequireHttpsMetadata = true;
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.Use(async (ctx, next) =>
+            {
+                var result = ctx.User?.Identity?.IsAuthenticated;
+                if (result.HasValue && result.Value)
+                {
+                    await next();
+                }
+                else
+                {
+                    ctx.Response.StatusCode = 403;
+                }
+            });
+
+            app.RunProxy(new ProxyOptions
+            {
+                Scheme = "http",
+                Host = "localhost",
+                Port = Configuration.GetValue<string>("BACKEND_SERVICE_PORT")
+            });
+        }
+    }
+}
