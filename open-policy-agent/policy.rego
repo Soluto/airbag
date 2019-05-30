4d1e4a86b4d8b9339426ac1c97efe6a41cfbf7b4
omerlh <omerl@soluto.com>
2019-05-30 08:55:06 +0300

    added support for authorization based on OPA

---
+++ open-policy-agent/policy.rego
@@ -0,0 +1,3 @@
+package protected_api.allow
+
+default allow=true
---
+++ src/OpenPolicyAgent/IOpenPolicyAgent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Airbag.Utils;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestEase;
+
+namespace Airbag.OpenPolicyAgent
+{
+    public class OpenPolicyAgentInput
+    {
+        [JsonProperty("path")]
+        public string[] Path { get; set; }
+
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonConverter(typeof(KeyValuePairConverter))]
+        [JsonProperty("query")]
+        public IEnumerable<KeyValuePair<string, string[]>> Query { get; set; }
+
+        [JsonConverter(typeof(KeyValuePairConverter))]
+        [JsonProperty("claims")]
+        public IEnumerable<KeyValuePair<string,string[]>> Claims { get; set; }
+    }
+
+    public class OpenPolicyAgentQueryRequest
+    {
+        [JsonProperty("input")]
+        public OpenPolicyAgentInput Input { get; set; }
+    }
+
+    public class OpenPolicyAgentQueryResponse
+    {
+        [JsonProperty("result")]
+        public bool? Result { get; set; }
+
+        [JsonProperty("decision_id")]
+        public string DecisionId { get; set; }
+    }
+
+    public interface IOpenPolicyAgent
+    {
+        [Post("v1/data/{query}")]
+        Task<OpenPolicyAgentQueryResponse> Query([Path(UrlEncode = false)] string query, [Body]OpenPolicyAgentQueryRequest request);
+    }
+}
